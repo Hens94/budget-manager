05efa01e8f3355bf3f3a9141c56c9a65bec9ee6b
Henry Gadea <henry_gadea@hotmail.com>
2024-09-05 15:06:32 -0600

    Create main template

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,17 @@
+import Header from "@/components/layout/Header";
+import Sidebar from "@/components/layout/Sidebar";
+import type { PropsWithChildren } from "react";
+
+const MainLayout = ({ children }: PropsWithChildren) => {
+  return (
+    <div className="grid grid-cols-1 grid-rows-layout h-screen">
+      <Header />
+      <div className="grid grid-cols-layout grid-rows-1">
+        <Sidebar />
+        <main className="bg-body">{children}</main>
+      </div>
+    </div>
+  );
+};
+
+export default MainLayout;
