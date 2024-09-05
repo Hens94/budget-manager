a2eeaaad7174fc059da8526feaed774b48cf547f
Henry Gadea <henry_gadea@hotmail.com>
2024-09-05 14:59:56 -0600

    Update styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,8 @@
 import type { Config } from "tailwindcss";
+import twColors from "tailwindcss/colors";
+
+const { lightBlue, warmGray, trueGray, coolGray, blueGray, ...colors } =
+  twColors;
 
 const config: Config = {
   content: [
@@ -7,7 +11,25 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      ...colors,
+      primary: " #308e87",
+      secondary: "#f39159",
+      success: "#3eb95f",
+      info: "#02a2b9",
+      warning: "#ea9200",
+      danger: "#e74b2b",
+      body: "#f9f7f6",
+      muted: "#aaa3a0",
+    },
+    extend: {
+      gridTemplateRows: {
+        layout: "77px 1fr",
+      },
+      gridTemplateColumns: {
+        layout: "253px 1fr",
+      },
+    },
   },
   plugins: [],
 };
